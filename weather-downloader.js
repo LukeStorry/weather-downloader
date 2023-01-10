037ab06d1bcba83e1a7bc3c8c545657a54930503
Luke Storry <luke.storry@letsdothis.com>
2023-01-10 13:35:16 +0000

    record full 24-image play animation

--- weather-downloader.js
+++ weather-downloader.js
@@ -1,11 +1,11 @@
 #!/usr/local/bin/node
-const { formatDistanceToNow, format, parse, add, differenceInHours } = require("date-fns");
+const { formatDistanceToNow, format, parse } = require("date-fns");
 const puppeteer = require("puppeteer");
-const { readdirSync, renameSync, unlinkSync } = require("fs");
+const { readdirSync, renameSync } = require("fs");
 
 const folder = "/Users/lukestorry/Pictures/wallpaper/";
-const threeDaysAgo = add(Date.now(), { days: -3 });
-const dateFormatString = "yyyy-MM-dd_HHmm-s.'png'";
+const date = new Date();
+const dateFormatString = "yyyy-MM-dd_HHmm-ss.'png'";
 
 const images = readdirSync(folder)
   .filter((filename) => filename.endsWith(".png"))
@@ -13,40 +13,44 @@ const images = readdirSync(folder)
   .map((filename) => ({ filename, date: parse(filename, dateFormatString, 0) }));
 
 console.log(`Downloading Windy.com images... (${format(Date.now(), "do MMM H:mm")})`);
-console.log(
-  `Currently ${images.length} images. ${
-    images.length > 0 ? "Last downloaded " + formatDistanceToNow(images.at(-1).date) + " ago" : ""
-  }`
-);
 
-const old = images.filter(({ date }) => date < threeDaysAgo);
-if (old.length > 0) {
-  old.forEach(({ filename }) => renameSync(folder + filename, folder + "old/" + filename));
-  console.log(`Archived ${old.length} images older than ${threeDaysAgo.toDateString()}`);
-}
-
-const dupes = images.filter(({ date }, i) =>
-  images.find(
-    ({ date: otherDate }, otherI) => i != otherI && date <= otherDate && differenceInHours(date, otherDate) == 0
-  )
-);
-if (dupes.length > 0) {
-  dupes.forEach(({ filename }) => unlinkSync(folder + filename));
-  console.log(`Deleted ${dupes.length} images that were duplicates`);
+if (images.length > 0) {
+  images.forEach(({ filename }) => renameSync(folder + filename, folder + "old/" + filename));
+  console.log(`Archived ${images.length} images, last download was ${formatDistanceToNow(images.at(-1).date)} ago.`);
 }
 
 const width = 3400,
   height = 2000,
-  clip = { x: 160, y: 150, width: width - 500, height: height - 187 };
+  clip = { x: 100, y: 150, width: width - 500, height: height - 187 };
+
 puppeteer.launch().then(async (browser) => {
   const page = await browser.newPage();
   await page.setViewport({ width: width, height: height });
-  await page.goto("https://www.windy.com/?54.828,-20.400,6", {
-    waitUntil: ["networkidle0", "networkidle2"],
-  });
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  const path = folder + format(Date.now(), dateFormatString);
-  await page.screenshot({ path, clip });
-  console.log("Downloaded:", path);
+  await page.goto("https://www.windy.com/?54.828,-18.400,6", { waitUntil: "networkidle0" });
+  await page.click('#playpause');
+  console.log(`Loaded page.`);
+
+
+  for (let i = 0; i < 24; i++) {
+    // Let animations load
+    await new Promise((resolve) => setTimeout(resolve, 2345));
+
+    // Screenshot
+    const path = folder + format(date.setSeconds(i), dateFormatString);
+    await page.screenshot({ path, clip });
+    console.log("Downloaded:", path);
+
+    // Click play button
+    await Promise.all([
+      page.click('#playpause.off'),
+      page.waitForNavigation({ waitUntil: "networkidle0" }),
+    ]);
+
+    // ~ 6 hours forwards
+    await new Promise((resolve) => setTimeout(resolve, 1080));
+
+    // Pause
+    await page.click('#playpause')
+  }
   await browser.close();
 });
